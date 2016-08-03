0535972f906b079471d9cbe287bd779e164ddf75
corderobot <rodriscor@gmail.com>
2016-08-03 08:24:09 -0600

    Mejoras
    Se le ha mejorado un poco la forma escrita del código, creando
    variables para las consultas en jQuery

--- codigo/assets/javascript/scripts.js
+++ codigo/assets/javascript/scripts.js
@@ -1,18 +1,20 @@
-var div;
-var div2;
+var $div;
+var $div2;
+var $global = $('.global');
+var $numero = $('#numero');
+var $colores = $('#colores');
 
 $(document).ready(function(){
 
 	$('#btn_ingresar').on('click', function(){
-		var num = $('#numero').val();
-		var color = $('#colores').val();
+		var num = $numero.val();
+		var color = $colores.val();
 		for (var i = 0; i < num; i++) {
-			div = $('<div>');
-			div.addClass("mostrar").css('background-color' , color);
-			div2 = $('<div>');
-			div2.addClass("contenedor");
-			(div2).append(div);
-			$('.global').append(div2);
+			$div = $('<div>');
+			$div.addClass("mostrar").css('background-color' , color);
+			$div2 = $('<div class="contenedor">');
+			($div2).append($div);
+			$global.append($div2);
 		};
 	});
 
@@ -21,4 +23,3 @@ $(document).ready(function(){
 	})
 
 });
-
